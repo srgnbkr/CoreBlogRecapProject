65c62f627a716942dd11611cc9b1ebff01402a19
Sergen Bakır <sbakir09@outlook.com>
2021-10-11 12:45:35 +0300

    11.10.2021

---
+++ CoreBlogWebUI/Controllers/CommentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogWebUI.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult CommentListByBlog()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
+    }
+}
